78771c44a8cf24d5f11af86fcc41cf5ba660be17
LGNDLeviathan <roitchie@gmail.com>
2025-04-05 16:25:15 +0100

    Adding spawning of door and dynamically moving back

--- Assets/CorridorSpawner.cs
+++ Assets/CorridorSpawner.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class CorridorSpawner : MonoBehaviour
 {
     public GameObject corridorPrefab;
-    public int corridorLength = 20;
-    private int lastZSpawned = 0;
+    public GameObject doorwayPrefab;
+    public int corridorLength = 5;
+    private int lastZSpawned = 10;
     private List<GameObject> spawnedCorridors = new List<GameObject>();
+    public Vector3 lastPlayerPos;
 
     public int buffer = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,14 +20,24 @@ public class CorridorSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(lastPlayerPos == null)
+        {
+            lastPlayerPos = GameManager.Instance.playerController.transform.position;
+        }
         //Get players position 
         Vector3 playerPos = GameManager.Instance.playerController.transform.position;
         //When the player passes some threshold. Spawn in a new corridor
-        if (Input.GetKeyDown(KeyCode.S))
+        if (playerPos.z + (buffer * corridorLength) > lastZSpawned)
         {
-            Instantiate(corridorPrefab, this.transform.position, Quaternion.identity);
-        }  
+            SpawnCorridor(lastZSpawned);
+        }
 
+        if(playerPos.z - lastPlayerPos.z > 0)
+        {
+            doorwayPrefab.transform.Translate(new Vector3(0, 0, playerPos.z - lastPlayerPos.z));
+            lastPlayerPos = playerPos;
+        }
+        
         //TODO: Delete old corridors? 
     }
 
